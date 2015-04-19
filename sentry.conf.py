ba0c919eedfe45ae0fd3a36e23de510981bcc3b3
George Whewell <georgerw@gmail.com>
2015-04-20 00:02:16 +0100

    config redis for all components

--- sentry.conf.py
+++ sentry.conf.py
@@ -3,7 +3,7 @@
 # you can inherit and tweak settings to your hearts content.
 from sentry.conf.server import *
 
-import os
+import os.path
 
 from psycopg2cffi import compat
 compat.register()
@@ -14,17 +14,135 @@ CONF_ROOT = os.path.dirname(__file__)
 import dj_database_url
 DATABASES = {'default': dj_database_url.config()}
 
+# You should not change this setting after your database has been created
+# unless you have altered all schemas first
+SENTRY_USE_BIG_INTS = True
+
+# If you're expecting any kind of real traffic on Sentry, we highly recommend
+# configuring the CACHES and Redis settings
+
+#############
+## General ##
+#############
+
+# The administrative email for this installation.
+# Note: This will be reported back to getsentry.com as the point of contact. See
+# the beacon documentation for more information.
+
+# SENTRY_ADMIN_EMAIL = 'your.name@example.com'
+SENTRY_ADMIN_EMAIL = ''
+
+###########
+## Redis ##
+###########
+
+# Generic Redis configuration used as defaults for various things including:
+# Buffers, Quotas, TSDB
+redis_url = urlparse.urlparse(os.environ.get('REDIS_URL'))
+
+SENTRY_REDIS_OPTIONS = {
+    'hosts': {
+        0: {
+            'host': redis_url.hostname,
+            'port': redis_url.port,
+        }
+    }
+}
+
+###########
+## Cache ##
+###########
+
+# If you wish to use memcached, install the dependencies and adjust the config
+# as shown:
+#
+#   pip install python-memcached
+#
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': ['127.0.0.1:11211'],
+#     }
+# }
+#
+# SENTRY_CACHE = 'sentry.cache.django.DjangoCache'
+
+SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
+
+###########
+## Queue ##
+###########
+
+# See http://sentry.readthedocs.org/en/latest/queue/index.html for more
+# information on configuring your queue broker and workers. Sentry relies
+# on a Python framework called Celery to manage queues.
+
+CELERY_ALWAYS_EAGER = False
+BROKER_URL = os.environ.get('REDIS_URL')
+
+#################
+## Rate Limits ##
+#################
+
+SENTRY_RATELIMITER = 'sentry.ratelimits.redis.RedisRateLimiter'
+
+####################
+## Update Buffers ##
+####################
+
+# Buffers (combined with queueing) act as an intermediate layer between the
+# database and the storage API. They will greatly improve efficiency on large
+# numbers of the same events being sent to the API in a short amount of time.
+# (read: if you send any kind of real data to Sentry, you should enable buffers)
+
+SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
+
+############
+## Quotas ##
+############
+
+# Quotas allow you to rate limit individual projects or the Sentry install as
+# a whole.
+
+SENTRY_QUOTAS = 'sentry.quotas.redis.RedisQuota'
+
+##########
+## TSDB ##
+##########
+
+# The TSDB is used for building charts as well as making things like per-rate
+# alerts possible.
+
+SENTRY_TSDB = 'sentry.tsdb.redis.RedisTSDB'
+
+##################
+## File storage ##
+##################
+
+# Any Django storage backend is compatible with Sentry. For more solutions see
+# the django-storages package: https://django-storages.readthedocs.org/en/latest/
+
+SENTRY_FILESTORE = 'django.core.files.storage.FileSystemStorage'
+SENTRY_FILESTORE_OPTIONS = {
+    'location': '/tmp/sentry-files',
+}
+
 ################
 ## Web Server ##
 ################
 
 # You MUST configure the absolute URI root for Sentry:
-SENTRY_URL_PREFIX = ''  # No trailing slash!
+SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX')  # No trailing slash!
+
+# If you're using a reverse proxy, you should enable the X-Forwarded-Proto
+# header and uncomment the following settings
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 SENTRY_WEB_HOST = '0.0.0.0'
-SENTRY_WEB_PORT = 8080
+SENTRY_WEB_PORT = 9000
 SENTRY_WEB_OPTIONS = {
-    'limit_request_line': 0,  # required for raven-js
-    'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
+    # 'workers': 3,  # the number of gunicorn workers
+    # 'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
 }
 
 #################
@@ -45,23 +163,13 @@ EMAIL_USE_TLS = False
 # The email address to send on behalf of
 SERVER_EMAIL = 'root@localhost'
 
+# If you're using mailgun for inbound mail, set your API key and configure a
+# route to forward to /api/hooks/mailgun/inbound/
+MAILGUN_API_KEY = ''
+
 ###########
 ## etc. ##
 ###########
-
-redis_url = urlparse.urlparse(os.environ.get('REDIS_URL'))
-
-CACHES = {
-        'default': {
-            'BACKEND': 'redis_cache.RedisCache',
-            'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
-            'OPTIONS': {
-                'DB': 0,
-        }
-    }
-}
-SENTRY_CACHE = 'sentry.cache.django.DjangoCache'
-
 
 # If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
 # Changing this value will result in all current sessions being invalidated
